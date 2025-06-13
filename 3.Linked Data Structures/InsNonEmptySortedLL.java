1c904c58d9bf6e1abb57740ce02151d3d600c4a7
JXPJXT <samjj317@gmail.com>
2025-06-13 12:47:15 +0530

    v17

---
+++ 3.Linked Data Structures/InsNonEmptySortedLL.java
@@ -0,0 +1,32 @@
+public class InsNonEmptySortedLL {
+    public static void main(String[] args) {
+        Node start = new Node(22);
+        Node p = start;
+        for (int i = 1; i < 5; i++) {
+            p = p.next = new Node(22 + 11 * i);
+        }
+
+        start = insert(start, 11);
+
+        Node current = start;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+    }
+
+    static Node insert(Node start, int x) {
+        if (start == null || start.data > x) {
+            Node newNode = new Node(x, start);
+            return newNode;
+        }
+
+        Node s = start;
+        while (s.next != null && s.next.data < x) {
+            s = s.next;
+        }
+
+        s.next = new Node(x, s.next);
+        return start;
+    }
+}
--- 3.Linked Data Structures/Node.java
+++ 3.Linked Data Structures/Node.java
@@ -1,8 +1,14 @@
-public class Node{
+public class Node {
     int data;
     Node next;
-    Node(int data){
+
+    Node(int data) {
         this.data = data;
         this.next = null;
+    }
+
+    Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
     }
 }
