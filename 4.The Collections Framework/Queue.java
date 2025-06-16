aa875dae6201dbf06d397b99b8001657941775e6
JXPJXT <samjj317@gmail.com>
2025-06-16 11:06:45 +0530

    v24

---
+++ 4.The Collections Framework/Queue.java
@@ -0,0 +1,7 @@
+public interface Queue<E> extends Collection<E>{
+    boolean offer(E o);
+    E poll();
+    E remove();
+    E peek();
+    E element();
+}
---
+++ 4.The Collections Framework/TestPairClass.java
@@ -0,0 +1,36 @@
+public class TestPairClass {
+    public static void main(String[] args) {
+        Pair<Month, Integer> christmas = new Pair<>(Month.DEC, 25);
+        System.out.println(christmas);
+        Month month = christmas.getFirst();
+        int day = christmas.getSecond();
+        System.out.printf("%d %s%n", day, month);
+    }
+}
+
+enum Month {
+    JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
+};
+
+class Pair<S, T> {
+    private final S first;
+    private final T second;
+
+    public Pair(S first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public S getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
+}
